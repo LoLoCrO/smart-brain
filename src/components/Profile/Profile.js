272d4c558028814de6ab026f901122b066042af1
lolocro <bosniclovro@gmail.com>
2020-01-07 14:25:40 +0100

    added suth

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,111 @@
+import React from 'react';
+import './Profile.css';
+
+class Profile extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            ...this.props.user
+        };
+    }
+
+    onFormChange = (event) => {
+        switch (event.target.name) {
+            case 'user-name':
+                this.setState({ name: event.target.value });
+                break;
+            case 'user-age':
+                this.setState({ age: event.target.value });
+                break;
+            case 'user-pet':
+                this.setState({ pet: event.target.value });
+                break;
+            default:
+                return;
+        }
+    }
+
+    onProfileUpdate = (data) => {
+        fetch(`http://localhost:3000/profile/${this.props.user.id}`, {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': window.sessionStorage.getItem('token')
+            },
+            body: JSON.stringify({ formInput: data })
+        })
+            .then(res => {
+                if (res.status === 200 || res.status === 304) {
+                    this.props.toggleModal();
+                    this.props.loadUser({ ...this.props.user, ...data });
+                }
+            })
+    }
+
+    render() {
+        const { user, toggleModal } = this.props;
+        const { name, age, pet } = this.state;
+        return (
+            <div className="profile-modal" >
+                <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-white">
+                    <main className="pa4 black-80 w-80">
+                        <img
+                            src="http://tachyons.io/img/logo.jpg"
+                            className="br-100 ba h3 w3 dib" alt="avatar"
+                        />
+                        <h1>{this.state.name}</h1>
+                        <h4>{`Images submitted: ${this.state.entries}`}</h4>
+                        <p>{`Member since: ${new Date(this.state.joined).toLocaleDateString()}`}</p>
+                        <hr />
+                        <label className="mt2 fw6" htmlFor="user-name">Name:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="pa2 ba w-100"
+                            placeholder={user.name}
+                            type="text"
+                            name="user-name"
+                            id="name"
+                        />
+                        <label className="mt2 fw6" htmlFor="user-age">Age:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="pa2 ba w-100"
+                            placeholder={user.age}
+                            type="number"
+                            name="user-age"
+                            id="age"
+                            min="0"
+                        />
+                        <label className="mt2 fw6" htmlFor="user-pet">Pet:</label>
+                        <input
+                            onChange={this.onFormChange}
+                            className="b pa2 ba w-100"
+                            placeholder={user.pet}
+                            type="text"
+                            name="user-pet"
+                            id="pet"
+                        />
+                        <div className="mt4" style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+                            <button className="b pa2 grow pointer hover-white w-40 bg-light-blue b--black-20"
+                                onClick={() => this.onProfileUpdate({ name, age, pet })}
+                            >
+                                Save
+                        </button>
+                            <button className="b pa2 grow pointer hover-white w-40 bg-light-red b--black-20"
+                                onClick={toggleModal}
+                            >
+                                Cancle
+                        </button>
+                        </div>
+                    </main>
+                    <div className="modal-close" onClick={toggleModal}>
+                        &times;
+                    </div>
+                </article>
+            </div>
+
+        )
+    }
+}
+
+export default Profile;
