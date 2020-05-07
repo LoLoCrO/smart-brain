fbfa40ca467e32dc9961ea9509bf7e7fc2d36d0f
lolocro <bosniclovro@gmail.com>
2020-05-07 19:36:33 +0200

    update

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
-      </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+class Rank extends Component {
+  constructor() {
+    super();
+    this.setState = {
+      emoji: ''
+    }
+  }
+
+  generateEmoji = (entries) => {
+    fetch(`https://lg08whl7f3.execute-api.eu-central-1.amazonaws.com/prod/rank?rank=${entries}`)
+      .then(res => res.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(console.log)
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='white f3'>
+          {`${this.props.name}, your current entry count is...`}
+        </div>
+        <div className='white f1'>
+          {this.props.entries}
+        </div>
+        <div className='white f1'>
+          {/* {`Rank badge: ${this.state.emoji}`} */}
+        </div>
+      </div >
+    );
+  }
 }
 
 export default Rank;
